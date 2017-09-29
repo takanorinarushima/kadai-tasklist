5dc7a4d482cfa641dc3c6595e3e447e0e52e52a2
Takanori Narushima <takanorinarushimatechacademy@gmail.com>
2017-09-29 07:04:24 +0000

    kadai-tasklist2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  validates :status, presence: true, length:{maximum:10}
+  validates :content, presence: true, length:{maximum: 255}
+  
+  # validates_presence_of :content, :status
+  # validates_length_of 
 end
